c52a48f3f69648709d35d200b393da26a0ea9e96
Edson Flavio <147329714+Edsonflaviobr@users.noreply.github.com>
2026-08-12 23:45:57 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -372,7 +372,7 @@ function stopInstructionsAudio() {
 }
 
 function tryPlayAudio() {
-  bgAudio.volume = 0.02;
+  bgAudio.volume = 0.05;
   bgAudio.play().catch(() => {});
 }
 
@@ -890,13 +890,13 @@ function stopCaseReader(restoreAudio = true) {
 
 function lowerGameAudioForReading() {
   if (!bgAudio.paused) {
-    bgAudio.volume = 0.00;
+    bgAudio.volume = 0.02;
   }
 }
 
 function restoreGameAudioAfterReading() {
   if (!bgAudio.paused) {
-    bgAudio.volume = 0.02;
+    bgAudio.volume = 0.05;
   }
 }
 
